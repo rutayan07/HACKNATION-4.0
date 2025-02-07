3e08ed3b167ce86cdf4f1528d81cabfeb1123c40
rutayan07 <patrarutayan@gmail.com>
2025-02-08 04:30:16 +0530

    Update 20250206044439_withered_gate.sql

--- Scripters/20250206044439_withered_gate.sql
+++ Scripters/20250206044439_withered_gate.sql
@@ -1,12 +1,3 @@
-/*
-  # Add phone number to charging sessions
-
-  1. Changes
-    - Add phone_number column to charging_sessions table
-    
-  2. Security
-    - Maintain existing RLS policies
-*/
 
 ALTER TABLE charging_sessions 
 ADD COLUMN IF NOT EXISTS phone_number text NOT NULL;
